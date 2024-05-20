fbc35729363dbef327b10284b57bc6b2f0eb9075
Evgeniy <eugtrader@gmail.com>
2024-05-20 21:47:27 +0300

    add grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Portfolio from "./pages/portfolio/Portfolio.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="wrapper">
+
+          <div className="main-grid">
+
+              <div className="box-item header-grid">header-grid</div>
+              <div className="box-item sidebar-grid">sidebar-grid</div>
+              <div className="box-item pages-grid">
+                  <Portfolio />
+              </div>
+              <div className="box-item footer-grid">footer-grid</div>
+
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
---
+++ src/pages/portfolio/Portfolio.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import style from './style.module.css'
+
+function Portfolio() {
+    const [count, setCount] = useState(0)
+
+    return (
+
+        <div>
+
+            <div className={style.pages}>pages-grid</div>
+
+        </div>
+
+    )
+}
+
+export default Portfolio
